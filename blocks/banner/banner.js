189889987cdbad9c4d38d14b53848dab09ab3b16
Luca Nerlich <luca.nerlich@googlemail.com>
2024-06-05 09:43:53 +0200

    add editorconfig, remove eslint

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,3 +1,6 @@
+import {createOptimizedPicture} from '../../scripts/aem.js';
+
 export default function decorate(block) {
-  console.log('block', block);
+  const image = block.querySelector('img');
+  image.closest('picture').replaceWith(createOptimizedPicture(image.src, image.alt, false, [{width: '750'}]));
 }
